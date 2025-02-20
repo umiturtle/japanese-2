b9a56edd06779f58ce29dc8590d5a08065fb7859
umiturtle <seaturtleumigame@gmail.com>
2025-02-20 12:00:03 +0000

    remake

--- japanese2/README.md
+++ japanese2/README.md
@@ -1,4 +1,4 @@
-# Points
+# Japanese2
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.8.
 
--- japanese2/src/app/app.component.spec.ts
+++ japanese2/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'points' title`, () => {
+  it(`should have the 'japanese2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('points');
+    expect(app.title).toEqual('japanese2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, points');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, japanese2');
   });
 });
